1a112991a019faaac79b9f56148e087616de7f12
caoc <ipod_nano@qq.com>
2019-03-19 17:53:46 +0800

    Site updated: 2019-03-19 17:53:46

--- js/ocean.js
+++ js/ocean.js
@@ -54,8 +54,8 @@
   };
   // 建议在移动端不初始化，其实 /search.xml 文件还挺大的，
   if ($('.local-search').size() && !isMobile.any()) {
-    $.getScript('/js/search.js', function () {
-      searchFunc("/search.xml", 'local-search-input', 'local-search-result');
+    $.getScript('/hexo/js/search.js', function () {
+      searchFunc("/hexo/search.xml", 'local-search-input', 'local-search-result');
     });
   }
 
